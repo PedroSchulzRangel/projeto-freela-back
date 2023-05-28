10e566570feaff0cd401394b0857bc842dd3678a
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-05-28 19:12:21 -0300

    id dos voos adicionado nas queries de busca de searchForFlight e searchForFilteredFlights

--- src/controllers/flights.controllers.js
+++ src/controllers/flights.controllers.js
@@ -60,3 +60,14 @@ export async function getFlightsByCityId (req, res) {
         res.status(500).send(error.message);
     }
 }
+
+export async function getFlightsById (req, res) {
+    
+    const {id} = req.params;
+
+    try{
+        
+    } catch (error){
+        res.status(500).send(error.message);
+    }
+}
--- src/routes/flights.route.js
+++ src/routes/flights.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { validateFlightsSchema } from "../middlewares/validateFlightsSchema.middleware.js";
 import { flightsSchema } from "../schemas/flights.schema.js";
-import { insertFlights, getFlightsByCityId} from "../controllers/flights.controllers.js";
+import { insertFlights, getFlightsByCityId, getFlightsById} from "../controllers/flights.controllers.js";
 
 const flightsRouter = Router();
 
 flightsRouter.post("/flights", validateFlightsSchema(flightsSchema), insertFlights);
 flightsRouter.get("/flights/city/:id", getFlightsByCityId);
+flightsRouter.get("/flights/:id", getFlightsById);
 
 export default flightsRouter;
